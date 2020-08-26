e3fb9a72ff210e3e80e719b95e7f7693a11ed6be
Nosferatu721 <elkintorres721@gmail.com>
2020-08-26 18:57:12 -0500

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let btnShare = document.getElementById("btnShare");
+let boxShare = document.getElementById("cont-share");
+
+btnShare.addEventListener("click", () => {
+  boxShare.classList.toggle("hide-share");
+});
